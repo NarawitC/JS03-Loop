ae81841d3595234193be79df114d3308f23373ff
NarawitC <narawit.developer@outlook.com>
2022-03-28 10:17:47 +0700

    Finish 2

--- 04/main.js
+++ 04/main.js
@@ -18,7 +18,8 @@ for (let i = 1; i <= 100; i++) {
 for (let i = 1; i <= 100; i++) {
   if (i % 2 === 0) {
     sum += i ** 2;
-  } else if (i % 3 === 0) {
+  }
+  if (i % 3 === 0) {
     sum -= i ** 2;
   }
 }
